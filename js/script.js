e71ec0db867959aa672d87349731428a8bda0c8f
Katerina <loginovakaty@gmail.com>
2020-10-19 16:08:30 +0300

    second

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ for (let i = 0; i < 2; i++) {
 }
 console.log(personalMovieDB);
 
-/* if (personalMovieDB.count < 10) {
+if (personalMovieDB.count < 10) {
     console.log('Просмотрено довольно мало фильмов');
 } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
     console.log('Вы классический киноман');
@@ -45,4 +45,4 @@ console.log(personalMovieDB);
     console.log('Вы киноман');
 } else {
     console.log('Произошла ошибка');
-} */
+}
