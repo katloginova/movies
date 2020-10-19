276b9fe76cc64a167231e6c808e2cf65a2135fd6
Katerina <loginovakaty@gmail.com>
2020-10-19 15:52:33 +0300

    second

--- js/script.js
+++ js/script.js
@@ -10,12 +10,39 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("What kind of last films do you like?", ""),
-      b = prompt("how much do you evaluate?", ""),
-      c = prompt("What kind of last films do you like?", ""),
-      d = prompt("how much do you evaluate?", "");
+//1-ый способ (мой)
+/* for (let i = 0; i < 2; i++) {
+    let a = prompt("What kind of last films do you like?", "");
+    while (a == null || a.length === 0 || a.length > 50) {
+        a = prompt("What kind of last films do you like?", "");
+    }
+    let b = prompt("how much do you evaluate?", "");
+    while (b == null || b.length === 0 || b.length > 50) {
+        b = prompt("how much do you evaluate?", "");
+    }
+    personalMovieDB.movies[a] = b;
+} */
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
+//2-ой способ (из урока)
+for (let i = 0; i < 2; i++) {
+    const a = prompt("What kind of last films do you like?", ""),
+          b = prompt("how much do you evaluate?", "");
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        i--;
+        console.log('error');
+    }
+}
 console.log(personalMovieDB);
+
+/* if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log('Вы классический киноман');
+} else if (personalMovieDB > 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+} */
